e6140c54279d8321c694c9a0773e360e34bf586d
hamma-eb <hamma.eb@gmail.com>
2022-11-08 20:17:53 +0100

    1

--- public/javascript/CASAS/mapa.php
+++ public/javascript/CASAS/mapa.php
@@ -1,14 +1,16 @@
 <?php
-$x=$_GET['mapa_x'];
-$y=$_GET['mapa_y'];
+$x=$_GET['x'];
+$y=$_GET['y'];
 //echo("X:".$x);
 //echo("Y:".$y);
 ?>
 <html>
 <body>
     <h1> CASAS</h1>
-
+    <?php
+    echo ('<input type ="hidden" id="cajaX" value="'.$x.'">');
+    echo ('<input type ="hidden" id="cajaY" value="'.$y.'">');
+    ?>
     <!-- CONTROLES -->
-    
 </body>
 <html>
